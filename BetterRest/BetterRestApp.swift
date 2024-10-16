7ebed35ee208de72e77d1a787f8808c213936e9e
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-10-16 08:43:19 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Krow on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
